d56b1a8e1ff98c54896b294d78bc4398b1c59c67
JoaquinGiacusa <joaquingiacusa.dev@gmail.com>
2022-05-21 00:20:07 -0300

    fix webhook 1

--- controllers/orders.ts
+++ controllers/orders.ts
@@ -1,7 +1,9 @@
 import { Order } from "models/order";
 import { createPreference } from "lib/mercadopago";
 import { productsIndex } from "lib/algolia";
-import { roundToNearestMinutes } from "date-fns";
+import { getMerchantOrder } from "lib/mercadopago";
+import { User } from "models/user";
+import { sgMail } from "lib/sendgrid";
 
 type CreateOrderRes = {
   url: string;
@@ -26,7 +28,6 @@ export async function createOrderAndSendUrlByMail(
       items: [
         {
           title: product.Name,
-          picture_url: "http://www.myapp.com/myimage.jpg",
           category_id: "car_electronics",
           quantity: 1,
           currency_id: "ARS",
@@ -40,7 +41,7 @@ export async function createOrderAndSendUrlByMail(
       external_reference: order.id,
       //esta url que pertenece a mi endopoint se ejecuta cuando se paga o intenta pagar
       notification_url:
-        "https://pagos-mp-amber.vercel.app/api/webhooks/mercadopago",
+        "https://desafio-modulo-9.vercel.app/api/webhooks/mercadopago",
     });
 
     return { url: pref.init_point, orderId: order.id };
@@ -62,4 +63,48 @@ export async function getOrderById(orderId) {
     throw "la order solicitada no existe";
   }
   return order.data;
+}
+
+async function sendPayNotificationEmail(to: string) {
+  const msg = {
+    to: to,
+    from: "joaquingiacusa.dev@gmail.com", // Use the email address or domain you verified above
+    subject: "Codigo de ingreso",
+    text: "code...",
+    html: "<strong>Tu pago se realizo con exito</strong>",
+  };
+
+  try {
+    await sgMail.send(msg);
+    return { message: "mensaje enviado" };
+  } catch (error) {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body);
+    }
+  }
+}
+
+export async function payNotification(id, topic) {
+  if (topic == "merchant_order") {
+    const order = await getMerchantOrder(id);
+
+    if (order.order_status == "paid") {
+      const orderId = order.external_reference;
+      const myOrder = new Order(orderId);
+      await myOrder.pull();
+      myOrder.data.status = "closed";
+      myOrder.data.externalORder = order;
+      await myOrder.push();
+
+      const user = new User(myOrder.data.userId);
+      const email = user.data.email;
+      sendPayNotificationEmail(email);
+      //sendEmailInterno("Alguien compró algo")
+      return "pago exitoso";
+    } else {
+      throw "error";
+    }
+  }
 }
--- pages/api/webhooks/mercadopago.ts
+++ pages/api/webhooks/mercadopago.ts
@@ -1,24 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getMerchantOrder } from "lib/mercadopago";
 import { Order } from "models/order";
+import { User } from "models/user";
+import { payNotification } from "controllers/orders";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const { id, topic } = req.query;
 
-  if (topic == "merchant_order") {
-    const order = await getMerchantOrder(id);
-    if (order.order_status == "paid") {
-      const orderId = order.external_reference;
-      const myOrder = new Order(orderId);
-      await myOrder.pull();
-      myOrder.data.status = "closed";
-      myOrder.data.externalORder = order;
-      await myOrder.push();
-      //sendEmail("Tu pago fue confirmado")
-      //sendEmailInterno("Alguien compró algo")
-      res.send("ok");
-    }
-  } else {
-    res.send("error");
+  try {
+    const result = await payNotification(id, topic);
+    res.send;
+  } catch (error) {
+    res.status(404).send({ message: error });
   }
+  // if (topic == "merchant_order") {
+  //   const order = await getMerchantOrder(id);
+  //   if (order.order_status == "paid") {
+  //     const orderId = order.external_reference;
+  //     const myOrder = new Order(orderId);
+  //     await myOrder.pull();
+  //     myOrder.data.status = "closed";
+  //     myOrder.data.externalORder = order;
+  //     await myOrder.push();
+
+  //     //sendEmail("Tu pago fue confirmado")
+  //     //sendEmailInterno("Alguien compró algo")
+  //     res.send("ok");
+  //   }
+  // } else {
+  //   res.send("error");
+  // }
 }
